bb8bd386baab33f2c3285a15859caa83135730a6
Neermay10 <74848380+Neermay10@users.noreply.github.com>
2021-03-17 16:38:17 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,37 +1,46 @@
 var hr, mn, sc;
-hr = hour();
-mn = minute();
-sc = second();
+
 
 function setup() {
-  createCanvas(400,400);
+  createCanvas(windowWidth,windowHeight);
 }
 
 function draw() {
-  background(255,255,255);
+  background(0);
   angleMode(DEGREES);
+  hr = hour();
+  mn = minute();
+  sc = second();
 
+  noFill();
 
   scAngle = map(sc,0,60,0,360);
   mnAngle = map(mn,0,60,0,360);
-  hrAngle = map(hr,0,24,0,360);
+  hrAngle = map(hr,0,12,0,360);
+
+  translate(windowWidth/2,windowHeight/2)
+  rotate(-90);
+  strokeWeight(20);
 
   push();
   rotate(scAngle);
   stroke(255,0,0);
   line(0,0,100,0);
+  arc(0,0,300,300,0,scAngle);
   pop();
 
   push();
   rotate(mnAngle); 
   stroke(0,255,0);
   line(0,0,70,0);
+  arc(0,0,300,300,0,mnAngle);
   pop();
 
   push();
   rotate(hrAngle);
   stroke(0,0,255); 
   line(0,0,50,0);
+  arc(0,0,300,300,0,hrAngle);
   pop();
 
 
